98c0d9740e43f5a7f2b7722584338346eaadf321
Dylan Baker <dylanmbaker@yahoo.com>
2022-02-21 23:25:55 -0600

    finished mvp

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,47 +1,28 @@
-// Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
-// middleware functions from `auth-middleware.js`. You will need them here!
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const User = require('../users/users-model');
 
+const { checkUsernameFree, checkPasswordLength, checkUsernameExists } = require('./auth-middleware');
 
-/**
-  1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
+router.post('/register', checkUsernameFree, checkPasswordLength, (req, res, next) => {
+    const { username, password } = req.body;
+    const passwordHash = bcrypt.hashSync(password, 8);
+    User.add({ username, password: passwordHash })
+        .then((addedUser) => {
+            res.status(200).json(addedUser);
+        })
+        .catch(next);
+});
 
-  response:
-  status 200
-  {
-    "user_id": 2,
-    "username": "sue"
-  }
-
-  response on username taken:
-  status 422
-  {
-    "message": "Username taken"
-  }
-
-  response on password three chars or less:
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
- */
-
-
-/**
-  2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-  response:
-  status 200
-  {
-    "message": "Welcome sue!"
-  }
-
-  response on invalid credentials:
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
- */
-
+router.post('/login', checkUsernameExists, (req, res, next) => {
+  const { password } = req.body;
+  if (bcrypt.compareSync(password, req.user.password)) {
+        req.session.user = req.user;
+        res.status(200).json({ message: `Welcome ${req.user.username}!` });
+    } else {
+        next({ status: 401, message: 'Invalid credentials' });
+    }
+});
 
 /**
   3 [GET] /api/auth/logout
@@ -59,5 +40,19 @@
   }
  */
 
- 
+router.get('/logout', (req, res, next) => {
+    if (req.session.user) {
+      req.session.destroy(err => {
+        if (err) {
+          next(err)
+        } else {
+          res.json({ message: 'logged out' });
+        }
+      })
+    } else {
+      res.json({ message: 'no session' });
+    }
+});
+
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
